226c05a485a65ce25da26a364245c05656353b0b
Alfred Lam <pakto1230@gmail.com>
2024-02-21 11:16:38 +0800

    done

--- src/ClassSection.java
+++ src/ClassSection.java
@@ -1,5 +1,61 @@
-
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 public class ClassSection {
-
+    private String subject;
+    private int capacity;
+    private int yearLevel;
+    private ArrayList<Student> students = new ArrayList();
+    public ClassSection(String subject, int capacity, int yearLevel){
+        this.subject = subject;
+        this.capacity = capacity;
+        this.yearLevel = yearLevel;
+    }
+    public String getSubject(){
+        return this.subject;
+    }
+    public void setSubject(String subject){
+        this.subject = subject;
+    }
+    public int getCapacity(){
+        return this.capacity;
+    }
+    public void setCapacity(int capacity){
+        this.capacity = capacity;
+    }
+    public int getYearLevel(){
+        return this.yearLevel;
+    }
+    public void setYearLevel(int yearLevel){
+        this.yearLevel = yearLevel;
+    }
+    public ArrayList<Student> getStudents(){
+        return this.students;
+    }
+    public void addStudent(Student student){
+        if(student.getYearLevel() == this.yearLevel && !isStudentEnrolled(student)) {
+            this.students.add(student);
+        }
+    }
+    public void removeStudent(Student student){
+        int i = 0;
+        for(Student s : students){
+            if(s.equals(students.get(i))){
+                students.remove(i);
+                break;
+            }
+            i++;
+        }
+    }
+    public boolean isStudentEnrolled(Student student){
+        for(Student s : students){
+            if(student.equals(s)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public String toString(){
+        return("ClassSection{subject='" + subject + "', capacity=" + capacity + ", yearLevel=" + yearLevel + ", students=" + students + "}");
+    }
 
 }
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,53 @@
 public class Student {
-
-
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private int yearLevel;
+    private static int studentCount = 1;
+    public Student(String firstName, String lastName, int age, int yearLevel){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.yearLevel = yearLevel;
+        this.id = studentCount;
+        studentCount++;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public int getYearLevel(){
+        return yearLevel;
+    }
+    public void setYearLevel(int yearLevel){
+        this.yearLevel = yearLevel;
+    }
+    public int getStudentCount(){
+        return studentCount;
+    }
+    public boolean equals(Student student){
+        if(this.firstName.equals(student.getFirstName()) && this.lastName.equals(student.getLastName()) && this.age == student.getAge() && this.yearLevel == student.getYearLevel()) return true;
+        else return false;
+    }
+    public String toString(){
+        return("Student{id=" + id + ", firstName='" + firstName + "', lastName='" + lastName + "', age=" + age + ", yearLevel=" + yearLevel + "}");
+    }
 }
